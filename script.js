6ce884b42c451d73d7f6c9e213487fb4fc6d9749
SeamDux <seba.merino@duocuc.cl>
2024-04-24 13:18:16 -0400

    Comenzando a crear las funciones para manejo de datos de compra

--- script.js
+++ script.js
@@ -10,6 +10,37 @@ loadMoreBtn.onclick = () => {
     if(currentitem >= boxes.length) {
         loadMoreBtn.style.display = 'none'
     }
+}
+
+//carrito
+const carrito = document.getElementById('carrito');
+const elementos1 = document.getElementById('lista-1');
+const lista = document.querySelector('#lista-carrito tbody');
+const VaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
 
+cargarEventListeners();
+
+function cargarEventListeners() {
+    elementos1.addEventListener('click', comprarElemento);
+    carrito.addEventListener('click', eliminarElemento);
+    VaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+}
+
+function comprarElemento (e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemento(elemento);
+    }
+}
+
+function leerDatosElemento(elemento) {
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('.precio').textContent,
+        id: elemento.querySelector('a').getAttribute('data-id')
+    }
+    insertarCarrito(infoElemento);
 }
