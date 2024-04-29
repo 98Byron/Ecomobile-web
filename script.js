2d4905c586421618158becc25d7a2e415ab08d63
SeamDux <seba.merino@duocuc.cl>
2024-04-29 15:02:45 -0400

    Funciones de agregar terminadas y programacion de nuevos productos

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ cargarEventListeners();
 function cargarEventListeners() {
     elementos1.addEventListener('click', comprarElemento);
     carrito.addEventListener('click', eliminarElemento);
-    VaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 }
 
 function comprarElemento (e) {
@@ -43,4 +43,42 @@ function leerDatosElemento(elemento) {
         id: elemento.querySelector('a').getAttribute('data-id')
     }
     insertarCarrito(infoElemento);
+}
+
+function insertarCarrito(elemento) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${elemento.imagen}" widith=100 height=150px >
+        </td>
+        <td>
+            ${elemento.titulo}
+        </td>
+        <td>
+            ${elemento.precio}
+        </td>
+        <td>
+            <a herf="#" class="borrar" data-id="${elemento.id}" >X</a>
+        </td>
+    `;
+    lista.appendChild(row);
+}
+
+function eliminarElemento(e) {
+    e.preventDefault();
+    let elemento,
+        elementoid;
+    
+    if(e.target.classList.contains('borrar')) {
+        e.target.parentElement.parentElement.remove();
+        elemento = e.target.parentElement.parentElement;
+        elementoid = elemento.querySelector('a').getAttribute('data-id')
+    }
+}
+
+function vaciarCarrito() {
+    while(lista.firstChild) {
+        lista.removeChild(lista.removeChild);
+    }
+    return false;
 }
